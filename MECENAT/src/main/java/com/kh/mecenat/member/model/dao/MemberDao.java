d346723770f24175c3954695275b73b6d32aa154
WonHoYoo1990 <you_yearning@hanmail.net>
2022-12-12 14:49:09 +0900

    설정완료 후 커밋

--- MECENAT/src/main/java/com/kh/mecenat/member/model/dao/MemberDao.java
+++ MECENAT/src/main/java/com/kh/mecenat/member/model/dao/MemberDao.java
@@ -17,5 +17,6 @@ public class MemberDao {
 	public int deleteMember(SqlSessionTemplate sqlSession, String userId) {
 		return sqlSession.update("memberMapper.deleteMember", userId);
 	}
+	//업로드용 주석
 
 }
