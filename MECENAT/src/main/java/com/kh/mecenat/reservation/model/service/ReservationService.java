04e12eb0b2c99b9bd7fe627aadfa93c3146cf558
Hyester <apricode@naver.com>
2022-12-19 11:47:53 +0900

    새로등록한 ui+ 기존내용 덧붙임(ui 반영전 기존 애니메) #혜원, 만제

--- MECENAT/src/main/java/com/kh/mecenat/reservation/model/service/ReservationService.java
+++ MECENAT/src/main/java/com/kh/mecenat/reservation/model/service/ReservationService.java
@@ -2,15 +2,22 @@ package com.kh.mecenat.reservation.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.mecenat.reservation.model.vo.Hall;
 import com.kh.mecenat.reservation.model.vo.Performance;
 import com.kh.mecenat.reservation.model.vo.RentApplication;
 
 public interface ReservationService {
 
+//	전체 공연 리스트 조회
 	ArrayList<Performance> selectPerformanceList();
 
+//	공연 세부내용 조회
 	Performance selectPerformance(int perfoNo);
 
+//	대관신청서 제출
 	int insertApplication(RentApplication ra);
+
+//	공연홀 정보조회
+	Hall selectHallInfo(String hallName);
 
 }
