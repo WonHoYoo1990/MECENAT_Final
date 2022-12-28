2a476d69a1113e902ce409898f9dc31b4d56f1fb
Hoya_ <Hoya_@DESKTOP-741B932>
2022-12-28 16:30:02 +0900

    다시..전으로 백업...?

--- MECENAT/src/main/java/com/kh/mecenat/notice/model/dao/NoticeDao.java
+++ MECENAT/src/main/java/com/kh/mecenat/notice/model/dao/NoticeDao.java
@@ -1,31 +1,5 @@
 package com.kh.mecenat.notice.model.dao;
 
-import java.util.ArrayList;
-
-import org.apache.ibatis.session.RowBounds;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.stereotype.Repository;
-
-import com.kh.mecenat.common.PageInfo;
-import com.kh.mecenat.notice.model.vo.Notice;
-
-@Repository
 public class NoticeDao {
-
-	//공지사항 총 개수
-	public int selectListCount(SqlSessionTemplate sqlSession) {
-		return sqlSession.selectOne("noticeMapper.selectListCount");
-	}
-	
-	//공지사항 리스트+페이칭 처리
-	public ArrayList<Notice> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
-		
-		int limit = pi.getBoardLimit();
-		int offset = (pi.getCurrentPage()-1) * limit;
-		
-		RowBounds rowBounds = new RowBounds(offset, limit);
-		
-		return (ArrayList)sqlSession.selectList("noticeMapper.selectList",null,rowBounds);
-	}
 
 }
